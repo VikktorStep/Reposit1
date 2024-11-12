7745708821c08d32782eadadfe5cfec902107ebe
Viktor Stepanov <vikktor.stepanov@gmail.com>
2024-11-13 00:33:25 +0300

    second commit

--- Reposit1/ContentView.swift
+++ Reposit1/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("My hello comm1it")
+            Text("My hello comm1asddit")
+            Text("1-2")
         }
         .padding()
     }
