f3ed90fa06b13cea854efa724fb19076024c83f2
Viktor Stepanov <vikktor.stepanov@gmail.com>
2024-11-12 23:51:33 +0300

    Initial Commit

---
+++ Reposit1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reposit1
+//
+//  Created by Виктор on 12.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reposit1/Reposit1App.swift
@@ -0,0 +1,17 @@
+//
+//  Reposit1App.swift
+//  Reposit1
+//
+//  Created by Виктор on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Reposit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
